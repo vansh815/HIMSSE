f06470df56a6594fe8ed18c24223e849355413b0
Vanshah <vanshah@iu.edu>
2020-09-23 20:39:53 -0400

    Initial setup

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import { Container } from "react-bootstrap";
+import { useAuth0 } from "@auth0/auth0-react";
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { NavBar, Footer, Loading, PrivateRoute } from "./components";
+import { Home, Profile, ExternalApi } from "./views";
+
+import "./App.css";
+
+const App = () => {
+  const { isLoading } = useAuth0();
+
+  if (isLoading) {
+    return <Loading />;
+  }
+
+  return (
+    <div id="app" className="d-flex flex-column h-100">
+      <NavBar />
+      <Container className="flex-grow-1 mt-5">
+        <Switch>
+        <Route path="/" exact component={Home} />
+          <PrivateRoute path="/profile" component={Profile} />
+          <PrivateRoute path="/external-api" component={ExternalApi} />
+        </Switch>
+      </Container>
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/views/home.js
@@ -0,0 +1,11 @@
+import React, { Fragment } from "react";
+
+import { Hero, Content } from "../components";
+
+const Home = () => (
+  <Fragment>
+    <Hero />
+  </Fragment>
+);
+
+export default Home;
