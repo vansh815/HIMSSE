696a6c97f5a96e051e64e5b833450fa8d1d90422
Andrew Olsen <aolsen6900@gmail.com>
2020-10-13 18:24:20 -0400

    Add files via upload

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -3,27 +3,7 @@ import Calendar from 'react-calendar'
 
 class Dashboard extends Component {
     render() {
-      let dataPie = {
-        labels: ["40%", "20%", "40%"],
-        series: [40, 20, 40]
-      }
-      let dataSales = {
-        labels: [
-          "9:00AM",
-          "12:00AM",
-          "3:00PM",
-          "6:00PM",
-          "9:00PM",
-          "12:00PM",
-          "3:00AM",
-          "6:00AM"
-        ],
-        series: [
-          [287, 385, 490, 492, 554, 586, 698, 695],
-          [67, 152, 143, 240, 287, 335, 435, 437],
-          [23, 113, 67, 108, 190, 239, 307, 308]
-        ]
-      }
+      
       return (
         <div className="content">
           <div className="container-fluid">
