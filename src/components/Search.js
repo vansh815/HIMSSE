d6e69f55cf6ecf75566d4c408d866232a8358a39
Jack Weickert <j.weickert32@gmail.com>
2020-10-20 17:38:45 -0400

    added 4 search inputs

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,30 +1,62 @@
-
 import React, { Component } from 'react'
-class Search extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {value: ''};
-    
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
 
-    handleSubmit(event) {
-        alert('A name was submitted: ' + this.state.value);
+const Search = (props) => {
+    const [state, setState] = React.useState({
+        fname: '',
+        lname: '',
+        spec: '',
+        loc: ''
+      });
+
+    const handleSubmit = (event) => {
+        alert( state.fname + ' ' + state.lname + ' ' + state.spec + ' ' + state.loc);
         event.preventDefault();
-        this.setState({value: ''});
+        setState({fname:'',
+                 lname: '',
+                 spec:'',
+                loc: ''});
+
     }
     
-    handleChange(event) {
-        this.setState({value: event.target.value.toLowerCase()});
+    const handleChange = (event) => {
+        //fname.value += event.target.value.toLowerCase();   
+        setState({
+            ...state,
+            [event.target.name]: event.target.value.toLowerCase()
+        });
     }
+    /*
+    const lhandleChange = (event) =>{
+        //lname.value += event.target.value.toLowerCase();
+        setState({
+            lname: event.target.value.toLowerCase()
+        });
+    }
+    
+    const lohandleChange = (event) => {
+        setState({
+            loc: event.target.value.toLowerCase()
+        });
+    }
+    const shandleChange = (event) => {
+        setState({
+            spec: event.target.value.toLowerCase()
+        });
+    }*/
+    /*
+    const renamePlaceholder = (state) => {
+        if state.fname =  
 
-    render() {
-        return(<form onSubmit={this.handleSubmit}>
-            <input type = "text" placeholder="Search..." name="search" value = {this.state.value} onChange={this.handleChange} />
+    }*/
+    return (
+        <form onSubmit={handleSubmit}>
+            <input type = "text" placeholder="Enter first name" name="fname" value = {state.fname} onChange={handleChange} />
+            <input type = "text" placeholder="Enter last name..." name="lname" value = {state.lname} onChange={handleChange} />
+            <input type = "text" placeholder="Enter specialization..." name="spec" value = {state.spec} onChange={handleChange} />
+            <input type = "text" placeholder="Enter location..." name="loc" value = {state.loc} onChange={handleChange} />
             <button type = "submit" value = "submit">Search</button> 
-        </form>)
-    }
+        </form>
+    )
 }
 
 export default Search
