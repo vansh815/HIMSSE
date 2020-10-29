853fd013d47fdc132e06393a8947c876fc588a2f
BrookeAllison <59938660+BrookeAllison@users.noreply.github.com>
2020-10-29 18:08:58 -0400

    Patient Profile.js working

--- src/views/profile.js
+++ src/views/profile.js
@@ -1,59 +1,119 @@
-import React from "react";
+import React, { Component } from 'react'
+import axios from 'axios'
 import { Container, Row, Col } from "react-bootstrap";
 import { Highlight } from "../components/navbar";
 
 import { useAuth0 } from "@auth0/auth0-react";
-
+ 
 const Profile = () => {
   const { user } = useAuth0();
   const { name, picture, email } = user;
 
+  const { getAccessTokenSilently } = useAuth0();
+  const token = getAccessTokenSilently();
+  const [state, setState] = React.useState({
+        first_name: '',
+        last_name: '',
+		user_email: {email},
+		insurance: 'insurance provider',
+        street_name: 'street name',
+    	city: 'city',
+    	state_name: 'state',
+    	zip_code: 'zip code'
+      });
+
+
+    const handleSubmit = (event) => {
+  		axios({
+            headers: {
+                authorization: 'Bearer ${token}'
+              },
+            method: 'get',
+            url: 'http://localhost:3010/patient/details',
+            data: {
+			  
+              first_name: state.first_name,
+              last_name: state.last_name,
+			  insurance: state.insurance,
+              street_name: state.street_name,
+    		  city: state.city,
+    		  state_name: state.state_name,
+    		  zip_code: state.zip_code,
+            }
+          });
+        setState({first_name: '',
+        last_name: '',
+		insurance: state.insurance,
+        street_name: state.street_name,
+    	city: state.city,
+    	state_name: state.state_name,
+    	zip_code: state.zip_code});
+        console.log({token});
+	}
+        
+
 	function getInsurance(){
-  		var insurance = prompt('Insurance:', 'Please insert Insurance');
-		if (insurance != null) {
-			{/* do something with the var, address */}
+  		state.insurance = prompt('Insurance:', 'Please insert Insurance');
+		if (state.insurance != null) {
+			axios.post('http://localhost:3010/patient/details', {
+				insurance: state.insurance
+				});
+			handleSubmit();
 		}		
 	}
 	
 	function getTitle(){
   		var title = prompt('Specialization:', 'Please insert your specialization');
 		if (title != null) {
-			{/* do something with the var, address */}
+			{/* do something with the var */}
 		}		
 	}
+	
 	
 	function getDescription(){
   		var description = prompt('About:', 'Please insert your relevant information');
 		if (description != null) {
-			{/* do something with the var, address */}
+			{/* do something with the var */}
 		}		
 	}
 	
 	function getStreetAddress(){
-  		var street = prompt('Street:', 'please enter your street address');
-		if (street != null) {
-			{/* do something with the var, address */}
+  		state.street_name = prompt('Street:', 'please enter your street address');
+		if (state.street_name != null) {
+			axios.post('http://localhost:3010/patient/details', {
+				street_name: state.street_name
+				});
+			handleSubmit();
 		}		
 	}
 	
 	function getCityAddress(){
-  		var city = prompt('City:', 'please enter your city');
-		if (city != null) {
-			{/* do something with the var, address */}
+  		state.city = prompt('City:', 'please enter your city');
+		if (state.city != null) {
+			axios.post('http://localhost:3010/patient/details', {
+				city: state.city
+				});
+			handleSubmit();
 		}		
 	}
 	
 	function getStateAddress(){
-  		var state = prompt('State:', 'please enter your state');
-		if (state != null) {
-			{/* do something with the var, address */}
+  		state.state_name = prompt('State:', 'please enter your state');
+		if (state.state_name != null) {
+			axios.post('http://localhost:3010/patient/details', {
+				state_name: state.state_name
+				});
+			handleSubmit();
 		}		
 	}
 	
 	function getZIPAddress(){
-  		var zip = prompt('Insurance:', 'please enter your zip code');
-		if (zip != null) {
-			{/* do something with the var, address */}
+  		state.zip_code = prompt('Insurance:', 'please enter your zip code');
+		if (state.zip_code != null) {
+			axios.post('http://localhost:3010/patient/details', {
+				zip_code: state.zip_code
+				});
+			handleSubmit();
 		}		
 	}
 
@@ -84,7 +144,7 @@ const Profile = () => {
       <Row className="card padding-bottom mb-3">
 		<p className="lead text-muted">{'\t'}Insurance:</p>
 		<p className="lead text-muted text-right">
-		name of insurance&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+		{state.insurance}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
 		<button onClick={getInsurance}>Edit</button>
 		</p>
 	  </Row>
@@ -115,22 +175,23 @@ const Profile = () => {
 	  <Row className="card padding-bottom mb-3">
 		<p className="lead text-muted">{'\t'}Address:</p>
 		<p className="lead text-muted text-right">
-		street address
+		{/*street address*/}
+		{state.street_name}
 		&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
 		<button onClick={getStreetAddress}>Edit</button>
 		</p>
 		<p className="lead text-muted text-right">
-		city
+		{state.city}
 		&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
 		<button onClick={getCityAddress}>Edit</button>
 		</p>
 		<p className="lead text-muted text-right">
-		state
+		{state.state_name}
 		&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
 		<button onClick={getStateAddress}>Edit</button>
 		</p>
 		<p className="lead text-muted text-right">
-		zip code
+		{state.zip_code}
 		&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
 		<button onClick={getZIPAddress}>Edit</button>
 		</p>
