8d21906d0cee65fdc575b87dcc0c316581f89405
Jack Weickert <j.weickert32@gmail.com>
2020-11-17 20:16:15 -0500

    added some front end code for booking

--- src/components/SearchProfile.js
+++ src/components/SearchProfile.js
@@ -9,9 +9,24 @@ class SearchProfile extends Component {
             lastname: "",
             speciality: "",
             location: "",
+            date: "",
+            time: ""
         };
-
     }
+
+    handleChange = (event) => {
+      this.setState({
+          ...this.state,
+          [event.target.name]: event.target.value
+      });
+    }
+
+    handleSubmit = (event) => {
+      event.preventDefault();
+      alert("Date: " + this.state.date + 
+      "\nTime: " + this.state.time);
+    }
+
     /*componentDidMount() {
         var item = this.props.history.location.state.item;
         console.log(this.props.history.location.state.item);
@@ -25,23 +40,46 @@ class SearchProfile extends Component {
     render(){
         return (
             <Container>
-              <Jumbotron style={{ backgroundColor: "#E6E6FA" }}>
-                <h1>Profile Page</h1>
-                <p>
-                  First Name : {this.state.firstname}
-                  {""}
-                  <br />
-                  Last Name : {this.state.lastname}
-                  {""}
-                  <br />
-                  Speciality : {this.state.speciality}
-                  <br />
-                  Location : {this.state.location}
-                </p>
-      
-                <p>
-                  <Button variant="info">Book Appointment</Button>
-                </p>
+              <Jumbotron style={{ backgroundColor: "#65aabf" }}>
+                <Row>
+                  <Col>
+                    <h1>Profile Page</h1>
+                    <p>
+                      First Name : {this.state.firstname}
+                      {""}
+                      <br />
+                      Last Name : {this.state.lastname}
+                      {""}
+                      <br />
+                      Speciality : {this.state.speciality}
+                      <br />
+                      Location : {this.state.location}
+                    </p>
+                  </Col>
+
+                  <Col>
+                    <Form onSubmit={this.handleSubmit}>
+                      <Form.Group>
+                        <Row>
+                          <Form.Label for="date">Date: </Form.Label>
+                          <Form.Control type="date" name="date" value={this.state.date} onChange={this.handleChange} />
+                        </Row>
+                        <br />
+                        <Row>
+                          <Form.Label for="time">Time: </Form.Label>
+                          <Form.Control type="time" step="1800" name="time" value={this.state.time} onChange={this.handleChange} />
+                        </Row>
+                        <br />
+                        <Button variant="dark" type="submit">Submit</Button>{' '}
+                          
+                      </Form.Group>
+                    </Form>
+          
+                    <p>
+                      <Button variant="dark">Book Appointment</Button>
+                    </p>
+                  </Col>
+                </Row>
               </Jumbotron>
             </Container>
           );
