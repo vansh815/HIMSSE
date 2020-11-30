bb9510d41b1ccea7b10c589cbf0e11c5ab13ac40
Jack Weickert <59937725+jweickert@users.noreply.github.com>
2020-11-30 14:53:51 -0600

    booking work

--- src/components/SearchProfile.js
+++ src/components/SearchProfile.js
@@ -18,131 +18,159 @@ const SearchProfile = () => {
     doctor_email : location.state.detail.email
   });
 
-    const handleChange = (event) => {
-      setState({
-        ...state,
-        [event.target.name]: event.target.value,
-      });
-    };
-    const callSecureApi = async (query) => {
-      const token = await getAccessTokenSilently();
-      const apiUrl = process.env.REACT_APP_API_URL;
-      console.log(query)
-      const searchquery = await axios({
-        headers: {
-            authorization: `Bearer ${token}`
-          },
-        method: 'get',
-        url: `${apiUrl}/booking/details`,
-        params: query
-      })
-      return searchquery
-    }
-    const handleSubmit = (event) => { // GET
-      
-      event.preventDefault();
-      let query=  {
-        doctor_email: state.doctor_email,
-      }
-      const searchquery = callSecureApi(query);
-      console.log("here");
-      console.log(searchquery);
-      
-      
+  const handleChange = (event) => {
+    setState({
+      ...state,
+      [event.target.name]: event.target.value,
+    });
+  };
 
-      searchquery.then(result => { 
-        let data = result.data;     
+  const callSecureApi = async (query) => {
+    const token = await getAccessTokenSilently();
+    const apiUrl = process.env.REACT_APP_API_URL;
+    console.log(query);
+    const searchquery = await axios({
+      headers: {
+          authorization: `Bearer ${token}`
+        },
+      method: 'get',
+      url: `${apiUrl}/booking/details`,
+      params: query
+    })
+    return searchquery
+  }
 
-        const appointment = state.date + "/" + state.time; // user's pending appt
-
-        var x = 0;
-        var i;
-        for (i = 0; i < data.length; i++) { // check availability of doctor
-          console.log("Loop Index: " + i);
-          if (data[i] === appointment) {
-            x = 1;
-            alert("Sorry, this time slot is not available. Please choose another time and try again.");
-          }
+  const callSecureApiPost = async (query) => {
+    const token = await getAccessTokenSilently();
+    const apiUrl = process.env.REACT_APP_API_URL;
+    console.log(query);
+    fetch(`${apiUrl}/booking/details` , {
+        method : "POST",
+        headers : {
+            "Authorization": `Bearer ${token}`,
+            "Content-Type" : "application/json",
+        },
+        body: query,
+    }).then((response) => {
+        if (!response.ok){
+            console.log("Error");
+        } else{
+            console.log("Success");
         }
+    });
+};
 
-        if (x === 0) {
-          alert("This time slot is available! Please click Confirm Appointment to continue.");
-        }
-
-       })
-      .catch(error => { console.error(error); })
-    };
-
-    const handleConfirm = (event) => { //POST
-      event.preventDefault();
-      const appointment = state.date + "/" + state.time;
-      alert(appointment)
+  const handleSubmit = (event) => { // GET
+    event.preventDefault();
+    let query=  {
+      doctor_email: state.doctor_email,
     }
-
-    useEffect(() => {
-      state.first_name = location.state.detail.first_name;
-      state.last_name = location.state.detail.last_name;
-      state.speciality = location.state.detail.speciality;
-      state.address = location.state.detail.address;
-      state.doctor_email = location.state.detail.email;
-    }, [location]);
-
-    /*componentDidMount() {
-        var item = this.props.history.location.state.item;
-        console.log(this.props.history.location.state.item);
-        this.setState({
-          firstname: item.firstname,
-          lastname: item.lastname,
-          speciality: item.speciality,
-          location: item.location,
-        });
-      }*/
+    const searchquery = callSecureApi(query);
+    console.log("here");
+    console.log(searchquery);
     
-    return (
-        <Container>
-          <Jumbotron style={{ backgroundColor: "#65aabf" }}>
-            <Row>
-              <Col>
-                <h1>Profile Page</h1>
-                <p>
-                  First Name : {state.first_name}
-                  {""}
-                  <br />
-                  Last Name : {state.last_name}
-                  {""}
-                  <br />
-                  Speciality : {state.speciality}
-                  <br />
-                  Location : {state.address}
-                </p>
-              </Col>
+    searchquery.then(result => { 
+      let data = result.data;     
 
-              <Col>
-                <Form onSubmit={handleSubmit}>
-                  <Form.Group>
-                    <Row>
-                      <Form.Label for="date">Date: </Form.Label>
-                      <Form.Control type="date" name="date" value={state.date} onChange={handleChange} />
-                    </Row>
-                    <br />
-                    <Row>
-                      <Form.Label for="time">Time: </Form.Label>
-                      <Form.Control type="time" step="1800" name="time" value={state.time} onChange={handleChange} />
-                    </Row>
-                    <br />
-                    <Button variant="dark" type="submit">Check Availability</Button>{' '}
-                      
-                  </Form.Group>
-                </Form>
-      
-                <p>
-                  <Button variant="dark" onClick={handleConfirm}>Confirm Appointment</Button>
-                </p>
-              </Col>
-            </Row>
-          </Jumbotron>
-        </Container>
-      );
+      const appointment = state.date + "/" + state.time; // user's pending appt
+
+      var x = 0;
+      var i;
+      for (i = 0; i < data.length; i++) { // check availability of doctor
+        console.log("Loop Index: " + i);
+        if (data[i] === appointment) {
+          x = 1;
+          alert("Sorry, this time slot is not available. Please choose another time and try again.");
+        }
+      }
+
+      if (x === 0) {
+        alert("This time slot is available! Please click Confirm Appointment to continue.");
+      }
+
+      })
+    .catch(error => { console.error(error); })
+  };
+
+  const handleConfirm = (event) => { //POST
+    event.preventDefault();
+    const appointment = state.date + "/" + state.time;
+
+    let query = {
+      doctor_email: state.doctor_email,
+      appointments: appointment
+    }
+
+    console.log(query);
+    const searchquery = callSecureApiPost(query);
+    console.log("here");
+    console.log(searchquery);
+  }
+
+  useEffect(() => {
+    state.first_name = location.state.detail.first_name;
+    state.last_name = location.state.detail.last_name;
+    state.speciality = location.state.detail.speciality;
+    state.address = location.state.detail.address;
+    state.doctor_email = location.state.detail.email;
+  }, [location]);
+
+  /*componentDidMount() {
+      var item = this.props.history.location.state.item;
+      console.log(this.props.history.location.state.item);
+      this.setState({
+        firstname: item.firstname,
+        lastname: item.lastname,
+        speciality: item.speciality,
+        location: item.location,
+      });
+    }*/
+  
+  return (
+    <Container>
+      <Jumbotron style={{ backgroundColor: "#65aabf" }}>
+        <Row>
+          <Col>
+            <h1>Profile Page</h1>
+            <p>
+              First Name : {state.first_name}
+              {""}
+              <br />
+              Last Name : {state.last_name}
+              {""}
+              <br />
+              Speciality : {state.speciality}
+              <br />
+              Location : {state.address}
+            </p>
+          </Col>
+
+          <Col>
+            <Form onSubmit={handleSubmit}>
+              <Form.Group>
+                <Row>
+                  <Form.Label for="date">Date: </Form.Label>
+                  <Form.Control type="date" name="date" value={state.date} onChange={handleChange} />
+                </Row>
+                <br />
+                <Row>
+                  <Form.Label for="time">Time: </Form.Label>
+                  <Form.Control type="time" step="1800" name="time" value={state.time} onChange={handleChange} />
+                </Row>
+                <br />
+                <Button variant="dark" type="submit">Check Availability</Button>{' '}
+                  
+              </Form.Group>
+            </Form>
+  
+            <p>
+              <Button variant="dark" onClick={handleConfirm}>Confirm Appointment</Button>
+            </p>
+          </Col>
+        </Row>
+      </Jumbotron>
+    </Container>
+  );
 }
 
 export default SearchProfile;
